177e62b9aeb134c6f2bef384f16e7996070ce774
unknown <abigoku@gmail.com>
2024-01-18 04:56:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    let audio = this.document.getElementsByTagName("audio");
+    if (audio.length) audio[0].play();
+})
+
+
+function getRandomNumberWithin(range) {
+   return Math.floor(Math.random() * range).toString() + "px";
+}
+
+
+
+// Change the CSS variable value every 2 seconds
+let nobtn = document.getElementById("nobtn")
+nobtn.addEventListener("click", () => {
+    nobtn.style.position = 'absolute';
+    nobtn.style.top = getRandomNumberWithin(450);
+    nobtn.style.left = getRandomNumberWithin(400);
+})
+
+let yesBtn = document.getElementById("yesbtn")
+yesBtn.addEventListener("click", () => {
+    const elements = document.getElementsByClassName("two")
+    if(elements.length){ 
+        elements[0].style.display = 'flex';
+        const firstPageElem = document.getElementsByClassName("one")
+        if( firstPageElem.length) firstPageElem[0].style.display = 'none';
+    }
+
+
+})
